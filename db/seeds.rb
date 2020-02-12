fb82410e6399d9cf4901610c23d368257d1afa32
maxime vanderwees <vanderwees.maxime@gmail.com>
2020-02-12 10:59:32 +0100

    wikinumous

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Starting the seed program'
+puts 'Detroy existing articles'
+
+Article.destroy_all
+
+puts 'Creating fake articles'
+
+10.times do
+  article = Article.new(title: Faker::Nation.language, content: Faker::ChuckNorris.fact)
+  article.save!
+end
+
+puts 'Articles added to the database'
